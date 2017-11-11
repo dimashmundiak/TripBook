4929ce772d73a06345e9694f4dc1db59c52f3b54
Dima Shmundiak <dimashmund@gmail.com>
2017-11-11 18:46:29 +0200

    added city detail component

---
+++ TripBook/TripBook.Client/src/app/components/city-detail/city-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-city-detail',
+  templateUrl: './city-detail.component.html',
+  styleUrls: ['./city-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CityDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
